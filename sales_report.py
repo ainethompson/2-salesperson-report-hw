b793806b2e1afbd8a45877f1b09d9621ed6363fb
Áine Thompson <ainegthompson@gmail.com>
2021-06-01 16:27:13 +0000

    Better pseudocode

--- sales_report.py
+++ sales_report.py
@@ -4,23 +4,29 @@
 salespeople = []
 melons_sold = []
 
-f = open('sales-report.txt')            #open file with salesperson data
+f = open('sales-report.txt')            #loop over each line in file object
 for line in f:
-    line = line.rstrip()
-    entries = line.split('|')           #separate each line wherever there's a |
+    line = line.rstrip()                #remove trailing whitespace
+    entries = line.split('|')           #create list of data
 
-    salesperson = entries[0]            #the first item of each line is the salesperson
-    melons = int(entries[2])            #the number of melons sold is the 3rd item of each line
+    salesperson = entries[0]            #get name of salesperson
+    melons = int(entries[2])            #get number of melons they've sold
 
-    if salesperson in salespeople:      #if the salesperson is in the list of salespeople
+# If the salesperson is already in `salespeople`, increment the no. of
+# melons they've sold.
+
+#otherwise, add the salesperson to the list of salespeaple
+#add the number of melons sold to the melons_sold list
+
+    if salesperson in salespeople:      
         position = salespeople.index(salesperson)
-                                        #define a variable called position, gets the index of that salesperson in the list of salespeople
-        melons_sold[position] += melons #adds however many melons to the melons_sold list at the position index
-    else:
-        salespeople.append(salesperson) #otherwise, add the salesperson to the list of salespeaple
-        melons_sold.append(melons)      #add the number of melons sold to the end of the melons_sold list
+                                       # find the position where the salesperson is stored in the list of salespeople
+        melons_sold[position] += melons 
+    else:                               # use that position to index into melons_sold
+        salespeople.append(salesperson) 
+        melons_sold.append(melons)     
 
 
-for i in range(len(salespeople)):      #for each number until the length of the salespeople list
+for i in range(len(salespeople)):      # Loop over indices of `salespeople` and use it to index into `salespeople` and `melons_sold`.
     print(f'{salespeople[i]} sold {melons_sold[i]} melons')
-                                        #print statement
+                                        #print the data
